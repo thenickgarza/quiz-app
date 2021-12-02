db0255c017232ef955ce48f70486f34e16b1f275
Nick Garza <thenickgarza14@gmail.com>
2021-12-02 16:03:02 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const startButton = document.querySelector(".start-btn")
+const questionContainerElement = document.getElementById(".question-container")
+const questionElement = document.getElementById('question')
+let shuffledQuestions, currentQuestionIndex
+const answerButtonElement = document.getElementById('answer-buttons')
+
+startButton.addEventListener("click", startGame)
+
+
+function startGame () {
+     console.log ("started");
+     startButton.remove()
+     shuffledQuestions = question.sort(() => Math.random() - .5)
+     // console.log(shuffledQuestions);
+     currentQuestionIndex = 0 
+     setNextQuestion ();
+}
+function showQuestion () {
+     questionElement.innerHTML = question
+     console.log(shuffledQuestions)
+}
+
+function setNextQuestion() {
+     showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function selectAnswer () {
+
+}
+
+var question = [
+    {
+        question: 'test question',
+        answers: [
+            { text: "test answer 1", correct: true },
+            { text: "test ansswer 2", correct: false },
+            { text: "test answer 3", correct: false },
+            { text: "test answer 4", correct: false}
+            ]
+    }
+]
+
+// var score = 0
+
+// var questions = [
+//     {
+//         questions: "test question",
+//         answers: [
+//             { text: "test answer", correct: true},
+//             { text: "test answer", correct: false}
+//         ]
+//     }
+// ]
+
+// var startButton = document.querySelector('.start-btn')
+// startButton.addEventListener("click", startGame)
+
+// var questionElement = document.querySelector(".starting-q")
+
+// let shuffledQuestions, currentQuestionIndex
+
+
+
+// function startGame () {
+//     console.log('started')
+//     startButton.remove()
+//     questionElement.remove()
+//     // shuffledQuestions = questions.sort(() => Math.random() - .5)
+//     currentQuestionIndex = 0 
+//     showQuestion()
+
+// }
+
+// function setwNextQuestion () {
+//     // showQuestion(shuffledQuestions[currentQuestionIndex])
+// }
+
+// function showQuestion () {
+// questionElement.innerText = questions
+// }
